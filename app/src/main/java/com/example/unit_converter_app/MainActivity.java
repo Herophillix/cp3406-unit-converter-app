904e6f1e2e9b3f2bbeaf6c1311ffb156ac9d71fa
James Makarios <jamesmakarioslo@gmail.com>
2022-04-12 21:32:20 +0800

    Color UI

--- app/src/main/java/com/example/unit_converter_app/MainActivity.java
+++ app/src/main/java/com/example/unit_converter_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.unit_converter_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] arraySpinner = new String[] {
+                "1", "2", "3", "4", "5", "6", "7"
+        };
+        Spinner s = (Spinner) findViewById(R.id.category_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
     }
 }
